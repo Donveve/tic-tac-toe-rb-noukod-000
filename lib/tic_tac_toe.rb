1a3b870284f52ccc2ed8fec9675a4560309d4ab5
Veniel Dorestave <veniel@noukod.com>
2018-09-03 20:20:55 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,4 +7,24 @@ WIN_COMBINATIONS = [
   [2,5,8],
   [0,4,8],
   [2,4,6]]
+  def display_board(
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+    puts 'display board'
+  #board output
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def input_to_index(move)
+   index = move.to_i - 1
+   index
+end
 
+def move(board, index, token)
+  board[index] = token
+  play(board)
+  
+end
